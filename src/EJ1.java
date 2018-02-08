de6bf47cdad01a1724e4b6b583ee2ee80842d0c8
CesarRodriguezM <cesar.rm97@gmail.com>
2018-02-08 10:07:41 +0100

    Prueba Para subir un proyecto

---
+++ src/EJ1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EJ1 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		
+		
+		Scanner Scan = new Scanner (System.in);
+		
+		String cadena = Scan.nextLine();
+		
+		
+		
+	}
+
+}
